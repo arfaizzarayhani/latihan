6009ba1c42942b11a1c01579c56d2cfae0e96ce6
Arfa <arfaizzarayhani@gmail.com>
2025-11-20 12:40:01 +0700

    coba 1

--- BruteForceExample.java
+++ BruteForceExample.java
@@ -1,56 +1,59 @@
 package latihan;
 
-import static java.lang.System.in;
 import java.util.Arrays;
-import java.util.Date;
+
 public class BruteForceExample {
-    public String username = new String();
-    public static String password = "ZZZZZ";
+
+    public static String password = "ARFAIZZA";
     public static char[] charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-    private static char[] currentGuess = new char[1];
+    private static char[] currentGuess;
 
-public static void bruteForce(){
-        String attempt = new String();
-        Date start = new Date();
-        while (true){
-            if (attempt.equals(password)){
-                Date end = new Date();
-                System.out.println("Password: " + attempt + "\nTotal time to crack: " + ((end.getTime() - start.getTime()) / 1000) + " seconds." + "\n");
-                break;
-            }
-            attempt = in.toString();
-            System.out.println("Tried: " + attempt);
-            in.increment();
-        }
-    }
-
-    public BruteForceExample(){
+    public BruteForceExample() {
+        currentGuess = new char[password.length()];
         Arrays.fill(currentGuess, charset[0]);
     }
 
-    public void increment(){
+    public void increment() {
         int index = currentGuess.length - 1;
-        while (index >= 0){
-            if (currentGuess[index] == charset[charset.length - 1]){
-                if (index == 0){
-                    currentGuess = new char[currentGuess.length + 1];
-                    Arrays.fill(currentGuess, charset[0]);
-                    break;
-                }
-                else{
-                    currentGuess[index] = charset[0];
-                    index--;
-                }
-            }
-            else{
-                currentGuess[index] = charset[Arrays.binarySearch(charset, currentGuess[index]) + 1];
-                break;
+
+        while (index >= 0) {
+            if (currentGuess[index] == charset[charset.length - 1]) {
+                currentGuess[index] = charset[0];
+                index--;
+            } else {
+                int pos = Arrays.binarySearch(charset, currentGuess[index]);
+                currentGuess[index] = charset[pos + 1];
+                return;
             }
         }
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return String.valueOf(currentGuess);
+    }
+
+    public static void bruteForce() {
+        BruteForceExample b = new BruteForceExample();
+        String attempt;
+        long start = System.currentTimeMillis();
+
+        while (true) {
+            attempt = b.toString();
+
+            if (attempt.equals(password)) {
+                long end = System.currentTimeMillis();
+                System.out.println("Password: " + attempt);
+                System.out.println("Total time to crack: " + (end - start) + " ms");
+                break;
+            }
+
+            System.out.println("Tried: " + attempt);
+            b.increment();
+        }
+    }
+
+    public static void main(String[] args) {
+        bruteForce();
     }
 }
